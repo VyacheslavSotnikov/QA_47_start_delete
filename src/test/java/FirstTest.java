ee49adb185a7c5b92c3dc09744e628d11e151de4
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-05-14 20:18:35 +0300

    created secondTest

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,6 +3,10 @@ import org.testng.annotations.Test;
 public class FirstTest {
     @Test
     public void firstTest(){
+    }
 
+    @Test
+    public void secondTest(){
+        System.out.println("it's second test");
     }
 }
